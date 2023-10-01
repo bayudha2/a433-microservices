10b5e067146831267407270b6088b54c80ae1a54
bayudha2 <bayudha.bd@gmail.com>
2023-10-01 17:25:41 +0700

    initial circleci config

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# build with docker image
+docker build -t bayudha2/karsajobs:latest
+echo ""
+
+# login to github packages
+echo $PAT | docker login ghcr.io --username bayudha2 --password-stdin
+echo ""
+
+# push tagged image to github packages
+docker push bayudha2/karsajobs:latest
+echo ""
